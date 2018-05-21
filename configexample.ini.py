3a557283b08d97a6947b96589b2f57f169af07b6
jsj1027 <jsj1027@rit.edu>
2018-05-21 15:15:17 -0400

    new config example file.

---
+++ configexample.ini.py
@@ -0,0 +1,5 @@
+[Main]
+#remove the py from the file or make a new ini file. The file type is ini
+token = <The Token for your bot goes here>
+announcements_channel = <channel id for your announcement channel>
+bot_test = <channel id for your bot_test channel>
