375986173e38eb12867b6de6f1e31883330a2f0e
jsj1027 <javisjohnson1@gmail.com>
2018-05-25 20:31:06 -0400

    changed to be more reflective of the bot

--- bot_events.py
+++ bot_events.py
@@ -11,25 +11,29 @@ class BotEvents:
         print('Work "{}" loaded'.format(self.__class__.__name__))
 
     async def on_member_join(self, member):
-            server = member.server
-            role = discord.utils.get(server.roles, name="Mortals")
-            channel = discord.utils.get(server.channels, name=config['channel_text']['introduction_channel_text'])
-            fmt = 'Welcome {0.mention} to the Universe 7 training camp! '
-            try:
-                await self.bot.send_message(channel, fmt.format(member))
-                print('new member joined!')
-                await self.bot.add_roles(member, role)
-                await self.bot.send_message(channel, "{You are now a mortal!")
-                print('given mortals role')
-            except discord.Forbidden:
-                await self.bot.send_message(channel, "I don't have perms to add roles.")
+        self.bot.wait_until_ready() #might break all the code, check if everything works then delete
+        server = member.server
+        role = discord.utils.get(server.roles, name="Mortals")
+        channel = discord.utils.get(server.channels, name=config['channel_text']['introduction_channel_text'])
+        fmt = 'Welcome {0.mention} to the Universe 7 training camp! '
+        try:
+            await self.bot.send_message(channel, fmt.format(member))
+            print('new member joined!')
+            await self.bot.add_roles(member, role)
+            await self.bot.send_message(channel, "{You are now a mortal!")
+            print('given mortals role')
+        except discord.Forbidden:
+            await self.bot.send_message(channel, "I don't have perms to add roles.")
 
     async def on_message(self, message):
+        self.bot.wait_until_ready() #might break all the code, check if everything works then delete
         if ":SuperJJ:" in message.content:
             await self.bot.send_message(message.channel, "JJ's power is going over 9000!")
+            await self.bot.process_commands(message) #might break all the code, check if everything works then delete
             print("power lvl increasing")
 
     async def on_message_edit(self, before, after):
+        self.bot.wait_until_ready() #might break all the code, check if everything works then delete
         if before.author.id == config['id']['whis_id']:
             pass
         else:
