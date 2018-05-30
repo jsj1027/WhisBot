4b36f4263a524c399e2657f260edd24a17a85820
jsj1027 <jsj1027@rit.edu>
2018-05-30 14:15:10 -0400

    added more things to possible, and small format changes

--- mod_commands.py
+++ mod_commands.py
@@ -9,10 +9,13 @@ config = configparser.ConfigParser()
 config.read("config.ini")
 server_owner = config['role_name']['server_owner']
 admin = config['role_name']['admin']
+whis = config['role_name']['whis']
 
-def possible(ctx, user, victim):
-    server = ctx.message.server
-    if victim.top_role.name == server_owner:
+
+def possible(user, victim):
+    if victim.top_role.name == whis:
+        return False
+    elif victim.top_role.name == server_owner:
         return False
     elif victim.top_role.name == admin:
         return False
@@ -21,12 +24,12 @@ def possible(ctx, user, victim):
     else:
         return True
 
+
 class ModCommands:
 
     def __init__(self, bot):
         self.bot = bot
         print('Work "{}" loaded'.format(self.__class__.__name__))
-
 
     @commands.command(pass_context=True, name="timeout")
     @mod_check()
@@ -36,10 +39,10 @@ class ModCommands:
         timeout_until = timeout_parse(command_string)
         server = ctx.message.server
         member = discord.utils.get(server.members, id=user)
-        if not possible(ctx, ctx.message.author, member):
+        if not possible(ctx.message.author, member):
             await self.bot.send_message(ctx.message.channel,
-                                        f"{ctx.message.author.mention} you are not allowed to use this on the Omni-King,"
-                                        f" other moderators, or yourself")
+                                        f"{ctx.message.author.mention} you are not allowed to use this on the"
+                                        f" Omni-King, me, other moderators, or yourself")
             print(f"{ctx.message.author} tried to use timeout on {member.name}")
             return
         timeout_role = discord.utils.get(server.roles, name=config['role_name']['timeout_role'])
@@ -47,14 +50,15 @@ class ModCommands:
                  f"This was on order of {ctx.message.author}"
         timeout_date = datetime.datetime.now()
         await self.bot.add_roles(member, timeout_role)
-        if member.voice_channel == None:
+        if member.voice_channel is None:
             pass
         else:
             channel = discord.utils.get(server.channels, name=config['channel_voice']['afk_channel'])
             await self.bot.move_member(member, channel)
         await self.bot.send_message(member, reason)
         await self.bot.send_message(ctx.message.channel,
-                                    f"{member.mention} was timed out until {timeout_date+timedelta(seconds=timeout_until)}")
+                                    f"{member.mention} was timed out until"
+                                    f" {timeout_date+timedelta(seconds=timeout_until)}")
         print(f"{member.name} was timed out by {ctx.message.author}")
         await asyncio.sleep(timeout_until)
         end_reason = f"{member.name} are no longer in my stasis field, try be better this time. " \
@@ -82,6 +86,7 @@ class ModCommands:
         self.bot.send_message(ctx.message.channel, msg)
         print(f"{user.name} with {user.id} tried to access {command} server owner permission.")
     """
+
 
 def setup(bot):
     bot.add_cog(ModCommands(bot))
