a6c64bdbddd656852318abe8754e2e037664c36e
jsj1027 <jsj1027@rit.edu>
2018-06-04 21:59:29 -0400

    Massive deletions as user and command parsing are taken care of so much easier now through list implementation. ctx.message.mentions and command_parse module

---
+++ helpers/timeout.py
@@ -0,0 +1,22 @@
+from datetime import timedelta
+
+
+def timeout_parse(commands):
+    weeks, days, hours, minutes, seconds, counter, num = 0, 0, 0, 0, 0, 0, 0
+    for command in commands:
+        if command.isdigit():
+            num = command
+        elif isinstance(command, str):
+            element = command.lower()
+            if element == 'wk':
+                weeks = int(num)
+            elif element == 'dy':
+                days = int(num)
+            elif element == 'hr':
+                hours = int(num)
+            elif element == 'min':
+                minutes = int(num)
+            elif element == 'sec':
+                seconds = int(num)
+    later = timedelta(weeks=weeks, days=days, hours=hours, minutes=minutes, seconds=seconds)
+    return later.total_seconds()
