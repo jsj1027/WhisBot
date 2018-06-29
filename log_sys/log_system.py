fcfe811175ff8b3d5a246c66f31a8fb631365bf4
jsj1027 <jsj1027@rit.edu>
2018-06-29 01:43:28 -0400

    Added new log file and moved some datetime actions to here instead of each individual message

--- log_sys/log_system.py
+++ log_sys/log_system.py
@@ -1,9 +1,11 @@
 import datetime
 
+
 def send_log(log_message, destination):
     file = open(f"log_files/{destination}_log.txt", "a+")
-    file.write(log_message+"\n")
+    file.write(log_message+f"    , DateTime: {datetime.datetime.now()}\n")
     file.close()
+
 
 def create_log_files():
     initialization = open("log_files/initialization_log.txt","a+")
@@ -11,11 +13,13 @@ def create_log_files():
     user_event = open("log_files/user_event_log.txt","a+")
     mod_command = open("log_files/mod_command_log.txt","a+")
     bot_event = open("log_files/bot_event_log.txt","a+")
+    bot_moderation = open("log_files/bot_moderation_log.txt","a+")
     initialization.close()
     user_command.close()
     user_event.close()
     mod_command.close()
     bot_event.close()
-    log_msg = f"Logging was set up properly, on {datetime.datetime.now}"
+    bot_moderation.close()
+    log_msg = f"Logging was set up properly."
     send_log(log_msg, "initialization")
 
