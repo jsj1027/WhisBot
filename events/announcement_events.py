ef39e769986ba6554b163401f9ccef5bb0197b6b
jsj1027 <jsj1027@rit.edu>
2018-07-19 16:13:46 -0400

    Added message logging

--- events/announcement_events.py
+++ events/announcement_events.py
@@ -18,14 +18,16 @@ class Announcements:
 
     async def on_message(self, message):
         channel = message.channel
-        announcement_channel = discord.utils.get(message.server.channels, name=config['channel_text']['announcement_channel_text'])
+        announcement_channel = discord.utils.get(message.server.channels,
+                                                 name=config['channel_text']['announcement_channel_text'])
         if channel is not announcement_channel:
-            if "@everyone" in message.content:
+            if message.mention_everyone:
                 await self.bot.delete_message(message)
-                await self.bot.send_message(channel, "You can't use @ everyone in this channel.")
+                await self.bot.send_message(channel, "You can't use '@everyone' in this channel.")
                 log_msg = f"{message.author} tried using @everyone in {channel} on {datetime.datetime.now}"
                 send_log(log_msg, log_location)
                 print(log_msg)
+
 
 def setup(bot):
     bot.add_cog(Announcements(bot))
