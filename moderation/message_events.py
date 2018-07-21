b0b7855806fb2aac1d1e9143a6959cf0fa210e5c
jsj1027 <jsj1027@rit.edu>
2018-07-21 05:37:30 -0400

    Small error not done.

--- moderation/message_events.py
+++ moderation/message_events.py
@@ -14,17 +14,17 @@ class MessageEvents:
         self.bot = bot
         print('Work "{}" loaded'.format(self.__class__.__name__))
 
-
     async def on_message(self, message):
         bad_words = check_contents(message)
         user_gained_points = point_assignment(bad_words)
         user_point_total = add_points(message.author.id, user_gained_points, bad_words)
         if user_point_total > 4:
-          self.bot.ban(message.author)
-          user_ban_reset(message.author.id)
+            self.bot.ban(message.author)
+            user_ban_reset(message.author.id)
           #send message saying you banned in pm
         else:
-          #send message warning them of their closeness to a ban in a pm
+            # send message warning them of their closeness to a ban in a pm
+            return
 
     async def on_message_delete(self, message):
         self.bot.wait_until_ready()
