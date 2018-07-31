d5138cda15b36525e24c4a6b05848536395ee978
jsj1027 <javisjohnson1@gmail.com>
2018-07-31 13:17:04 -0400

    Complete file restructuring and method renaming

--- helpers/dictionary.py
+++ helpers/dictionary.py
@@ -1,3 +1,6 @@
+import json
+from pathlib import Path
+"""
 black_list = {
     "arian": 0.5,
 
@@ -133,46 +136,67 @@ black_list = {
 }
 
 white_list = {
-    "sure": 0
+    "placeholder": 0
 }
 
 #values are [[bad words used list], total points, banned_x_times]
 users = {}
+"""
 
+database_folder = Path("database_files/")
+black_list_filename = database_folder / "black_list.json"
+white_list_filename = database_folder / "white_list.json"
+users_filename = database_folder / "users.json"
 
-def check_user_dict(user):
+if black_list_filename.exists():
+    with open(black_list_filename, "r+") as f:
+        black_list = json.load(f)
+else:
+    black_list = open(black_list_filename, "r+")
+if white_list_filename.exists():
+    with open(white_list_filename, "r+") as f:
+        white_list = json.load(f)
+else:
+    white_list = open(white_list_filename, "r+")
+if users_filename.exists():
+    with open(users_filename, "r+") as f:
+        users = json.load(f)
+else:
+    users = open(users_filename, "r+")
+
+def check_user_database(user):
     if user in users:
         return users[user]
     else:
         return False
 
 
-def change_user_dict_info(user_id_number, user_info):
+def change_user_database_info(user_id_number, user_info):
     users[user_id_number] = user_info
     return
 
 
-def check_black_list_dict(word):
+def add_user_to_user_database(user_id_number, bad_words_used, points_to_add):
+    users[user_id_number] = [[bad_words_used], points_to_add, 0]
+
+
+def check_black_list_database(word):
     if word in black_list:
         return black_list[word]
     else:
         return False
 
 
-def add_user_to_dict(user_id_number, bad_words_used, points_to_add):
-    users[user_id_number] = [[bad_words_used], points_to_add, 0]
-
-
-def check_white_list_dict(word):
+def check_white_list_database(word):
     if word in white_list:
         return white_list[word]
     else:
         return False
 
-
 def check_black_list_intersection(message_set):
     #return message_set.intersection(black_list)
     return message_set.keys() & black_list.keys()
+
 
 def check_white_list_intersection(message_set):
     #return message_set.intersection(white_list)
