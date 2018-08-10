3dad33a28ec894a145e4855ceb9ebc9ff95680d0
jsj1027 <jsj1027@rit.edu>
2018-08-10 15:55:35 -0400

    Refactored some function call names and fixed some returns

--- helpers/message_checker.py
+++ helpers/message_checker.py
@@ -8,7 +8,7 @@ def check_contents(message):
         message_set[word] = word
     message_set = check_black_list_intersection(message_set)
     if not message_set:
-        return
+        return set()
     message_white = check_white_list_intersection(message_set)
     if not message_white:
         for word in message_white:
@@ -19,14 +19,14 @@ def check_contents(message):
 def point_assignment(message_set):
     point_total = 0
     for word in message_set:
-      point_total += check_black_list_database(word)
+        point_total += check_black_list_database(word)
     return point_total
 
 
 def add_points(user_id_number, points_to_add, bad_words_used):
     total = 0
-    if check_user_database(user_id_number):
-        user_info = check_user_database(user_id_number)
+    if check_if_user_in_database(user_id_number):
+        user_info = check_if_user_in_database(user_id_number)
         user_info[1] += points_to_add
         user_info[0] += bad_words_used
         total = float(user_info[1])
@@ -38,10 +38,10 @@ def add_points(user_id_number, points_to_add, bad_words_used):
 
 def user_ban_reset(user_id_number):
 
-    user_info = check_user_database(user_id_number)
+    user_info = check_if_user_in_database(user_id_number)
     user_info[1] = 0
     user_info[2] += 1
     change_user_database_info(user_id_number, user_info)
     return
 
-#create some text file and load it with the users name saing heyyyy this nigga banned.
+# create some text file and load it with the users name saing heyyyy this nigga banned.
