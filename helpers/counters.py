20968eac876544500ca7364769f821a60e12db31
jsj1027 <jsj1027@rit.edu>
2018-09-17 21:51:42 -0400

    Fixed potato event by checking for the write unicode character Fixed counters by having proper data type switching

--- helpers/counters.py
+++ helpers/counters.py
@@ -1,21 +1,16 @@
 from pathlib import Path
 
-counter_file_folder = Path("database_files")
-potato_count_file = None
-
-
-def load_potato_count():
-    global potato_count_file
-    potato_count_path = counter_file_folder / "potato_count.txt"
-    potato_count_file = open(potato_count_path)
+database_folder = Path("database_files")
+potato_count_file = database_folder / "potato_count.txt"
+#potato_count_file = open(potato_count_file)
 
 
 def get_potato_count():
-    return potato_count_file.read()
+    global potato_count_file
+    return potato_count_file.read_text()
 
 
 def update_potato_count():
-    global potato_count_file
-    current_count = potato_count_file.read()
-    updated_count = current_count + 1
-    potato_count_file.write(updated_count)
+    current_count = get_potato_count()
+    updated_count = int(current_count) + 1
+    potato_count_file.write_text(str(updated_count))
