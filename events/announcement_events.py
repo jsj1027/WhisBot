04c6a15d392ee4f4495dd31f1b1c7ba271bcd24c
jsj1027 <javisjohnson1@gmail.com>
2018-09-22 19:59:31 -0400

    Removed unused import statement Added try&except block     For less crashing Updated code to work with rewrite

--- events/announcement_events.py
+++ events/announcement_events.py
@@ -1,5 +1,4 @@
 import discord
-from discord.ext import commands
 import configparser
 from log_sys.log_system import send_log
 import datetime
@@ -17,20 +16,21 @@ class Announcements:
         print('Work "{}" loaded'.format(self.__class__.__name__))
 
     async def on_message(self, message):
-        if message.mention_everyone:
-            channel = message.channel
-            announcement_channel = discord.utils.get(message.server.channels,
-                                                     name=config['channel_text']['announcement_channel_text'])
-            if channel is not announcement_channel:
-                await self.bot.delete_message(message)
-                await self.bot.send_message(channel, "You can't use '@everyone' in this channel.")
-                log_msg = f"{message.author} tried using @everyone in {channel} on {datetime.datetime.now}"
-                send_log(log_msg, log_location)
-                print(log_msg)
-            else:
-                pass
-
-
+        try:
+            if message.mention_everyone:
+                channel = message.channel
+                announcement_channel = discord.utils.get(message.guild.channels,
+                                                         id=config['channel_text']['announcement_channel_text'])
+                if channel is not announcement_channel:
+                    await message.delete()
+                    await channel.send("You can't use @ everyone in this battleground, it is annoying!"
+                                       " Use @ here if you really need a call to arms.")
+                    log_msg = f"{message.author} tried using @everyone in {channel} on {datetime.datetime.now}"
+                    send_log(log_msg, log_location)
+                else:
+                    pass
+        except Exception as error:
+            send_log(str(error), log_location)
 
 def setup(bot):
     bot.add_cog(Announcements(bot))
