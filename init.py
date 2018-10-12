40b780fa46b9faddaee8f2465b80ff2450129df2
jsj1027 <javisjohnson1@gmail.com>
2018-10-12 15:20:54 -0700

    checking new implementation of Base values/class/init

--- init.py
+++ init.py
@@ -2,61 +2,58 @@
 
 import discord
 from discord.ext import commands
-import configparser
 from log_sys.log_system import *
+from initialization import Base_class
 
 # config for token credit to https://github.com/nh-server/Kurisu
-config = configparser.ConfigParser()
-config.read("config.ini")
+Base = Base_class.Base()
+Base.load_config('config.ini')
+config = Base.get_config()
+
 
 log_location = 'initialization'
 
 bot = commands.Bot(command_prefix="!")
 
 create_log_files()
-works = [
-    'events.bot_events',
-    'events.announcement_events',
-    'cogs.bot_commands'#,
-  #  'cogs.mod_commands',
-  #  'moderation.message_events'
 
-]
+training_programs = Base.get_training_programs()
 
-failed_works = []
+failed_training_programs = Base.get_failed_training_programs()
 
 # Credit goes to https://github.com/nh-server/Kurisu/blob/master/run.py
 """
 loads the extra files for the bot(cogs, event calls, etc).
 """
-for work in works:
+for program in training_programs:
     try:
-        bot.load_extension(work)
+        bot.load_extension(program)
     except ValueError:
         print(TypeError)
     except Exception as x:
-        msg = f'{work} failed to come train the champions. The Omni-King will not be pleased.\n{type(x).__name__}: {x}'
+        msg = f'{program} failed to come train the champions.' \
+              f' The Omni-King will not be pleased.\n{type(x).__name__}: {x}'
         send_log(msg, log_location)
-        failed_works.append([work, type(x).__name__, x])
+        failed_training_programs.append([program, type(x).__name__, x])
+
 
 @bot.event
-async def on_ready():
+async def on_ready(self):
     try:
-        guild = bot.get_guild(id=int(config['id']['guild_id']))
-        channel = bot.get_channel(id=int(config['channel_text']['bot_test_text']))
-        if channel in guild.channels:
-            pass
-        else:
-            send_log(str(e), log_location)
-        bot_info = await bot.application_info()
+        guild = self.bot.get_guild(id=int(self.config['id']['guild_id']))
+        channel = self.bot.get_channel(id=int(self.config['channel_text']['bot_test_text']))
+        if channel not in guild.channels:
+            raise ValueError
+        bot_info = await self.bot.application_info()
         bot_name = bot_info.name
-        message = f"'Hmmm, training begins now!'\n" \
-                  f"'Ho Ho Ho, my name is {bot_name}, " \
-                  f"and this is the realm of the Omni-King!' - Whisbot\n"
+        message = f"'Hmmm, training begins now!'\n 'Ho Ho Ho, my name is {bot_name}," \
+                  f" and this is the realm of the Omni-King!' - WhisBot\n"
         embed = discord.Embed(title='Whis is online', description=message)
         await channel.send(embed=embed)
-        send_log(message, log_location)
+        send_log(message, self.log_location)
+    except ValueError:
+        send_log(str(f"{channel} not in {guild}"), self.log_location)
     except Exception as e:
-        send_log(str(e), log_location)
+        send_log(str(e), self.log_location)
 
 bot.run(config['token']['token'])
---
+++ initialization/Base_class.py
@@ -0,0 +1,36 @@
+# Whisbot by jsj1027/kaloshade
+
+import configparser
+from log_sys.log_system import *
+
+
+class Base:
+    # config for token credit to https://github.com/nh-server/Kurisu
+    config = configparser.ConfigParser()
+
+    log_location = 'initialization'
+
+    training_programs = [
+        'cogs.events.bot_events',
+        'cogs.events.announcement_events',
+        'cogs.commands.bot_commands'  # ,
+        #  'cogs.mod_commands',
+        #  'moderation.message_events'
+    ]
+
+    failed_training_programs = []
+
+    def get_config(self):
+        return self.config
+
+    def load_config(self, file_location):
+        self.config.read(file_location)
+
+    def get_training_programs(self):
+        return self.training_programs
+
+    def get_failed_training_programs(self):
+        return self.failed_training_programs
+
+    def set_failed_training_programs(self, failed_programs):
+        self.failed_training_programs = failed_programs
