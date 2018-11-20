3595b4150c2fc5005c7655e104df814be5d2a0ac
jsj1027 <jsj1027@rit.edu>
2018-11-19 23:47:18 -0500

    Refactored Uses Base Class Sends proper logs

--- cogs/events/bot_events.py
+++ cogs/events/bot_events.py
@@ -1,10 +1,10 @@
 import discord
-import configparser
+from initialization import Base_class
 from log_sys.log_system import *
 from helpers.counters import update_potato_count
 
-config = configparser.ConfigParser()
-config.read("config.ini")
+Base = Base_class.Base()
+config = Base.config
 
 log_location = "user_event"
 
@@ -24,28 +24,28 @@ class BotEvents:
                 log_msg = f"{message.author} triggered bot_events.on_message event on {datetime.datetime.now()}"
                 send_log(log_msg, log_location)
         except Exception as e:
-            print(str(e))
+            send_log(str(e), log_location)
 
     async def on_member_join(self, member):
-    #TODO Maybe Fixed needs testing
+        # TODO Maybe Fixed needs testing
         try:
             guild = member.guild
             role = guild.get_role(int(config['role_name']['mortals']))
             channel = self.bot.get_channel_id(int(config['channel_text']['introduction_channel_text']))
             msg = f'Welcome {member.mention} to the Universe 7 training camp! '
             await channel.send(msg)
-            new_mem = f'{member.name} joined {member.server} on {datetime.datetime.now()}'
-            send_log(new_mem, log_location)
+            new_mem_msg = f'{member.name} joined {member.server} on {datetime.datetime.now()}'
+            send_log(new_mem_msg, log_location)
             await member.add_roles(role, reason=f"User joined the guild {guild}")
-            await channel.send(content="You are now recognized by the Omni-King as a mortal wishing to social, grow, and"
-                                       "most importantly train in his realm! "
+            await channel.send(content="You are now recognized by the Omni-King as a mortal wishing to social, grow,"
+                                       " and most importantly train in his realm! "
                                        "Enjoy your stay, I will be watching over you")
-            given_role = f"{member.name} given Mortal role in {member.server} on {datetime.datetime.now()}"
-            send_log(given_role, log_location)
+            given_role_msg = f"{member.name} given Mortal role in {member.server} on {datetime.datetime.now()}"
+            send_log(given_role_msg, log_location)
         except discord.Forbidden:
             send_log("I don't have permission to add roles.", log_location)
-        except Exception:
-            send_log(Exception)
+        except Exception as e:
+            send_log(str(e), log_location)
 
 
 def setup(bot):
