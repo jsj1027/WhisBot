9ea06821fc697efbb580b547e41920639c110206
jsj1027 <javisjohnson1@gmail.com>
2018-11-23 19:15:53 -0500

    some skeleton code is now actual code

--- cogs/events/exp_events.py
+++ cogs/events/exp_events.py
@@ -2,6 +2,7 @@ import discord
 from pathlib import Path
 from log_sys.log_system import send_log
 from initialization import Base_class
+from helpers.user_exp_database_manager import *
 
 Base = Base_class.Base()
 config = Base.config
@@ -18,14 +19,16 @@ class RankedLadder:
         print(f'Work "{self.__class__.__name__}" loaded')
 
     async def on_ready(self):
-        #call ready function of user_exp_database_manager.py
+        ready()
 
     async def on_message(self, message):
-        user = message.author
-        user_checked_value = xp_check(user)
-        if user_checked_value:
+        userID = message.author.id
+        userName = message.author.name
+        if not check_user_in_exp_database(userID):
+            add_new_user_to_exp_database(userID, userName)
+        if not can_user_get_exp(userID):
             return
-        user_exp = user_exp_return()
+        user_exp = get_user_exp(userID)
         match = role_matches_current_exp(user.top_role, user_exp)
         if not match:
             return
