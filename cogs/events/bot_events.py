a1e1e8cb766e302c6db8c2664c11b1234e285144
jsj1027 <jsj1027@rit.edu>
2018-12-10 11:05:36 -0500

    On Member Join fixed and tested.
    Bot now properly greets new people, and gives them Mortals role automatically.

--- cogs/events/bot_events.py
+++ cogs/events/bot_events.py
@@ -27,20 +27,19 @@ class BotEvents:
             send_log(str(e), log_location)
 
     async def on_member_join(self, member):
-        # TODO Maybe Fixed needs testing
         try:
             guild = member.guild
             role = guild.get_role(int(config['role_name']['mortals']))
-            channel = self.bot.get_channel_id(int(config['channel_text']['introduction_channel_text']))
+            channel = self.bot.get_channel(int(config['channel_text']['introduction_channel_text']))
             msg = f'Welcome {member.mention} to the Universe 7 training camp! '
             await channel.send(msg)
-            new_mem_msg = f'{member.name} joined {member.server} on {datetime.datetime.now()}'
+            new_mem_msg = f'{member.name} joined {guild.name} on {datetime.datetime.now()}'
             send_log(new_mem_msg, log_location)
-            await member.add_roles(role, reason=f"User joined the guild {guild}")
+            await member.add_roles(role, reason=f"User joined the guild {guild.name}")
             await channel.send(content="You are now recognized by the Omni-King as a mortal wishing to social, grow,"
                                        " and most importantly train in his realm! "
                                        "Enjoy your stay, I will be watching over you")
-            given_role_msg = f"{member.name} given Mortal role in {member.server} on {datetime.datetime.now()}"
+            given_role_msg = f"{member.name} given Mortal role in {guild.name} on {datetime.datetime.now()}"
             send_log(given_role_msg, log_location)
         except discord.Forbidden:
             send_log("I don't have permission to add roles.", log_location)
