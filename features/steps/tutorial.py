fb02a44ce133de287052f4619d5a06e451e2a259
Javis Johnson <jsj1027@g.rit.edu>
2019-04-01 20:52:48 -0400

    sick of this fucking crap

--- features/steps/tutorial.py
+++ features/steps/tutorial.py
@@ -1,4 +1,6 @@
 from behave import *
+https://stackoverflow.com/questions/40022220/attempted-relative-import-beyond-toplevel-package/40022629
+#sick of this crap fix it with this 
 
 @given("we have behave installed")
 def step_impl(context):
